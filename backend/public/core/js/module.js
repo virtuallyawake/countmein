7cd4a84a5fab16152ac54d0dc5f8233ccda90cfe
wemakemarketingmagic <we@makemarketingmagic.com>
2016-07-02 22:30:08 +0200

    Took out some 3rd party dependencies

--- backend/public/core/js/module.js
+++ backend/public/core/js/module.js
@@ -1,3 +1,3 @@
 /* File: app/core/js/module.js */
 
-var weApp = angular.module("weApp", ['ngSanitize', 'ngAnimate', 'smart-table', 'ui.grid', 'ui.bootstrap', 'ui.router', 'ngFileUpload', 'ngToast']);
+var weApp = angular.module("weApp", ['ngSanitize', 'ngAnimate', 'smart-table', 'ui.bootstrap', 'ui.router', 'ngFileUpload', 'ngToast']);
